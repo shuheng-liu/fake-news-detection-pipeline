74ff7834e08529affa9c835d2a3b1106c8c91e83
Shuheng Johnny Liu <wish1104@outlook.com>
2018-09-18 04:54:01 +0800

    wrapped the process into a function, calling the function via cmdline
    Signed-off-by: Shuheng Johnny Liu <wish1104@outlook.com>

--- get_embeddings.py
+++ get_embeddings.py
@@ -7,13 +7,75 @@ from tools import DocumentEmbedder, DocumentSequence
 from nltk.corpus import stopwords
 from string import punctuation
 
+
+def get_embeddings(input, output, column='title', model='d2v', vec_size=300, pretrained=None, win_size=5, min_count=5,
+                   dm=0, epochs=20, normalizer=None, scorer='count'):
+    df = pd.read_csv(input)
+    raw_docs = df[column].values
+
+    docs = DocumentSequence(raw_docs, clean=True, sw=stopwords.words('english'), punct=punctuation)
+    embedder = DocumentEmbedder(docs, pretrained_word2vec=pretrained)
+
+    print('computing embeddings')
+
+    model = model  # type: str
+    if model.lower() == 'd2v':
+        out_name = "d2v(vecsize={}, winsize={}, mincount={}, {}, epochs={}).pkl".format(
+            vec_size, win_size, min_count, "dm" if dm else "dbow", epochs
+        )
+        embeddings = embedder.get_doc2vec(vectors_size=int(vec_size),
+                                          window=int(win_size),
+                                          min_count=int(min_count),
+                                          dm=int(dm),
+                                          epochs=int(epochs))
+
+    elif model.lower() == "nd2v":
+        out_name = "nd2v(normalizer={}).pkl".format(normalizer)
+        embeddings = embedder.get_naive_doc2vec(normalizer=normalizer)
+
+    elif model.lower() == "onehot":
+        out_name = "onehot(scorer={}).pkl".format(scorer)
+        embeddings = embedder.get_onehot(scorer=scorer)
+
+    elif model.lower() == "fasttext":
+        out_name = "fasttext().pkl"
+        embeddings = embedder._fast_text()  # not yet implemented
+
+    else:
+        print("unrecognized model, using naive doc2vec as fallback")
+        out_name = "nd2v(normalizer={}).pkl".format(normalizer)
+        embeddings = embedder.get_naive_doc2vec(normalizer=normalizer)
+
+    if isinstance(embeddings, list):  # if the embeddings is in a list, stack them into a 2-D numpy array
+        try:
+            embeddings = np.stack(emb if isinstance(emb, np.ndarray) else np.zeros(vec_size) for emb in embeddings)
+        except ValueError as e:
+            print(e)
+            print("embeddings will be saved in the form of a list")
+
+    print("embeddings computed")
+
+    # dump the embedding matrix on disk
+    try:
+        os.makedirs(output)
+    except FileExistsError:
+        print("Parent Dir Existent")
+    finally:
+        out_name = column + "-" + out_name
+        out_path = os.path.join(output, out_name)
+        with open(out_path, "wb") as f:
+            print("storing embeddings in {}".format(out_path))
+            pkl.dump(embeddings, f)
+            print("embeddings stored")
+
+
 if __name__ == '__main__':
     # control arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", required=True,
                         help="path to read csv file")
     parser.add_argument("--output", required=True,
-                        help="dir to dump embeddigns, NOT including filename, created if non-existent")
+                        help="dir to dump embeddings, NOT including filename, created if non-existent")
     parser.add_argument("--column", default="title",
                         help="which column to select from the csv file, default is `title`")
     parser.add_argument("--model", default="d2v",
@@ -41,56 +103,6 @@ if __name__ == '__main__':
     opt = parser.parse_args()
     print(opt)
 
-    df = pd.read_csv(opt.input)
-    raw_docs = df[opt.column].values
-
-    docs = DocumentSequence(raw_docs, clean=True, sw=stopwords.words('english'), punct=punctuation)
-    embedder = DocumentEmbedder(docs, pretrained_word2vec=opt.pretrained)
-
-    print('computing embeddings')
-
-    model = opt.model  # type: str
-    if model.lower() == 'd2v':
-        out_name = "d2v(vecsize={}, winsize={}, mincount={}, {}, epochs={}).pkl".format(
-            opt.vec_size, opt.win_size, opt.min_count, "dm" if opt.dm else "dbow", opt.epochs
-        )
-        embeddings = embedder.get_doc2vec(vectors_size=int(opt.vec_size),
-                                          window=int(opt.win_size),
-                                          min_count=int(opt.min_count),
-                                          dm=int(opt.dm),
-                                          epochs=int(opt.epochs))
-
-    elif model.lower() == "nd2v":
-        out_name = "nd2v(normalizer={}).pkl".format(opt.normalizer)
-        embeddings = embedder.get_naive_doc2vec(normalizer=opt.normalizer)
-
-    elif model.lower() == "onehot":
-        out_name = "onehot(scorer={}).pkl".format(opt.scorer)
-        embeddings = embedder.get_onehot(scorer=opt.scorer)
-
-    elif model.lower() == "fasttext":
-        out_name = "fasttext().pkl"
-        embeddings = embedder._fast_text()  # not yet implemented
-
-    else:
-        print("unrecognized model, using naive doc2vec as fallback")
-        out_name = "nd2v(normalizer={}).pkl".format(opt.normalizer)
-        embeddings = embedder.get_naive_doc2vec(normalizer=opt.normalizer)
-
-    if isinstance(embeddings, list):  # if the embeddings is in a list, stack them into a 2-D numpy array
-        embeddings = np.stack(embeddings)
-
-    print("embeddings computed")
-
-    # dump the embedding matrix on disk
-    try:
-        os.makedirs(opt.output)
-    except FileExistsError:
-        print("Parent Dir Existent")
-    finally:
-        out_name = opt.column + "-" + out_name
-        out_path = os.path.join(opt.output, out_name)
-        with open(out_path, "wb") as f:
-            print("storing embeddings in {}".format(out_path))
-            pkl.dump(embeddings, f)
-            print("embeddings stored")
+    get_embeddings(opt.input, opt.output, column=opt.column, model=opt.model, vec_size=opt.vec_size,
+                   pretrained=opt.pretrained, win_size=opt.win_size, min_count=opt.min_count, dm=opt.dm,
+                   epochs=opt.epochs, normalizer=opt.normalizer, scorer=opt.scorer)
