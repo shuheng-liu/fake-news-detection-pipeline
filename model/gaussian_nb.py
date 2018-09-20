d4d7e453fa3af3e454ad195cc8b560e3364d432a
YinQiaoyi <867208887@qq.com>
2018-09-20 22:30:39 +0800

    A few changes in parameters dictionary

--- model/gaussian_nb.py
+++ model/gaussian_nb.py
@@ -2,4 +2,4 @@ from sklearn.naive_bayes import GaussianNB
 import numpy as np
 
 model = GaussianNB()
-param_dist = {"prior": [None, np.array([0.5, 0.5]), np.array([0.1, 0.9]),np.array([0.3, 0.7]), np.array([0.7, 0.3])]}
+param_dist = {"priors": [None, np.array([0.5, 0.5]), np.array([0.1, 0.9]),np.array([0.3, 0.7]), np.array([0.7, 0.3])]}
--- model/random_forest.py
+++ model/random_forest.py
@@ -2,11 +2,11 @@ from scipy.stats import randint as sp_randint
 from sklearn.ensemble import RandomForestClassifier
 
 model = RandomForestClassifier()
-param_dist = {"n_estimators": [20, 30, 40, 50],
-              "max_depth": [3, 5, 7, 9, 10, None],
-              "max_features": sp_randint(1, 20),
-              "min_samples_split": sp_randint(2, 12),
-              "min_samples_leaf": sp_randint(1, 12),
+param_dist = {"n_estimators": [20, 30, 40],
+              "max_depth": [3, 7, None],
+              "max_features": sp_randint(1, 12),
+              "min_samples_split": sp_randint(2, 10),
+              "min_samples_leaf": sp_randint(1, 10),
               "bootstrap": [True, False],
               "criterion": ["gini", "entropy"]}
 
