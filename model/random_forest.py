3c12e162a68e97b6584c9d29aa007159a585f5a6
YinQiaoyi <867208887@qq.com>
2018-09-20 22:32:41 +0800

    A few changes in parameters dictionary.

--- model/random_forest.py
+++ model/random_forest.py
@@ -2,7 +2,7 @@ from scipy.stats import randint as sp_randint
 from sklearn.ensemble import RandomForestClassifier
 
 model = RandomForestClassifier()
-param_dist = {"n_estimators": [20, 30, 40],
+param_dist = {"n_estimators": [10, 20, 30, 40],
               "max_depth": [3, 7, None],
               "max_features": sp_randint(1, 12),
               "min_samples_split": sp_randint(2, 10),
