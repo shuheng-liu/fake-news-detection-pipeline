62aac2f845e92427abd46438b7448f9fd5ced28a
Shuheng Johnny Liu <wish1104@outlook.com>
2018-09-20 22:37:44 +0800

    modifications to params

--- model/random_forest.py
+++ model/random_forest.py
@@ -2,7 +2,7 @@ from scipy.stats import randint as sp_randint
 from sklearn.ensemble import RandomForestClassifier
 
 model = RandomForestClassifier()
-param_dist = {"n_estimators": [20, 30, 40, 50],
+param_dist = {"n_estimators": [10, 20, 30, 40, 50],
               "max_depth": [3, 5, 7, 9, 10, None],
               "max_features": sp_randint(1, 20),
               "min_samples_split": sp_randint(2, 12),
