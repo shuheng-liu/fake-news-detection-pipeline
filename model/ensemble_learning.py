990f6a8c4c1f00e7841c53cb545384b843d776a4
Shuheng Liu <wish1104@outlook.com>
2018-11-03 05:27:16 +0800

    deleted unused class by another contributor
    unused imports removed as well

--- model/ensemble_learning.py
+++ model/ensemble_learning.py
@@ -1,75 +1,7 @@
-from mlxtend.classifier import EnsembleVoteClassifier
-from mlxtend.plotting import plot_decision_regions
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-
-from sklearn.metrics import classification_report, f1_score
-from sklearn import model_selection
+from sklearn.metrics import f1_score
 from model.input_specific_model import InputSpecificModel as Voter
 
 import numpy as np
-
-
-class EnsembleLearning:
-    def __init__(self, classifiers, labels):
-        self.classifiers = classifiers
-        self.labels = labels
-
-    def ensemble_vote_classifier(self, x_train, y_train, x_test, y_test, weights_list=None, voting="soft"):
-        if weights_list == None:
-            weights_list = [1] * len(self.classifiers)
-
-        # initialize the ensemble learning classifier
-        eclf = EnsembleVoteClassifier(clfs=self.classifiers,
-                                      weights=weights_list, voting=voting)
-
-        # show the result of every classifier
-        for clf, label in zip(self.classifiers, self.labels):
-            scores = model_selection.cross_val_score(clf, x_train, y_train,
-                                                     cv=5,
-                                                     scoring='f1')
-            print("F1-score: %0.3f (+/- %0.3f) [%s]"
-                  % (scores.mean(), scores.std(), label))
-        # show the result of ensemble voting
-        scores = model_selection.cross_val_score(eclf, x_train, y_train, cv=5, scoring='f1')
-        print("F1-score: %0.3f (+/- %0.3f) [%s]" % (scores.mean(), scores.std(), 'EnsembleVoting'))
-
-        # build the ensemble model and make the prediction
-        eclf.fit(x_train, y_train)
-        y_pred = eclf.predict(x_test)
-
-        # Report the metrics
-        target_names = ['Real', 'Fake']
-        print("EnsembleVoteClassifier:")
-        print(classification_report(y_true=y_test, y_pred=y_pred, target_names=target_names, digits=3))
-
-    def ensemble_weighted_voting(self, x_train, y_train, x_test, y_test, weights_list=None):
-        y_pred = []
-        for model in self.classifiers:
-            model.fit(x_train, y_train)
-            y_pred.append(model.predict(x_test))
-
-        if weights_list == None:
-            weights_list = [1] * len(self.classifiers)
-
-        y_ensemble_pred = []
-        for idx in range(len(x_test)):
-            vote_sum = 0.0
-            weight_sum = 0.0
-            for itr in range(len(self.classifiers)):
-                weight_sum += weights_list[itr]
-                vote_sum += weights_list[itr] * y_pred[itr][idx]
-            vote_sum = vote_sum / weight_sum
-            # obtain the ensemble result
-            if vote_sum >= 0.5:
-                y_ensemble_pred.append(1)
-            else:
-                y_ensemble_pred.append(0)
-
-        # Report the metrics
-        target_names = ['Real', 'Fake']
-        print("EnsembleWeightedVoting:")
-        print(classification_report(y_true=y_test, y_pred=y_ensemble_pred, target_names=target_names, digits=3))
 
 
 class EnsembleVoter:
