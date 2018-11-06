660e5c67b388c705a544de9ccc39b7d3c37446ea
Shuheng Liu <wish1104@outlook.com>
2018-11-06 11:30:40 +0800

    renamed internal imports to avoid name conflicts

--- model/__init__.py
+++ model/__init__.py
@@ -1,27 +1,27 @@
 from .ensemble_learning import EnsembleVoter
 from .input_specific_model import InputSpecificModel
 from .hypertuned_models import (
-    qda,
-    gdb,
-    knn,
-    gnb,
-    lg,
-    mlp,
-    rf,
-    svc,
+    qda as hypertuned_qda,
+    gdb as hypertuned_gdb,
+    knn as hypertuned_knn,
+    gnb as hypertuned_gnb,
+    lg as hypertuned_lg,
+    mlp as hypertuned_mlp,
+    rf as hypertuned_rf,
+    svc as hypertuned_svc,
     classifiers as hypertuned_classifiers,
 )
 
 __all__ = [
     EnsembleVoter,
     InputSpecificModel,
-    qda,
-    gdb,
-    knn,
-    gnb,
-    lg,
-    mlp,
-    rf,
-    svc,
+    hypertuned_qda,
+    hypertuned_gdb,
+    hypertuned_knn,
+    hypertuned_gnb,
+    hypertuned_lg,
+    hypertuned_mlp,
+    hypertuned_rf,
+    hypertuned_svc,
     hypertuned_classifiers,
 ]
